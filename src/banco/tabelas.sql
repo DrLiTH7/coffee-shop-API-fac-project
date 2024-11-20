ba0ad15d196f864f01fc9cd0f52a3d37d6625248
DrLiTH7 <lith.yakame@gmail.com>
2024-11-20 18:04:40 -0300

    adicionado no dump as tabelas avaliações e carinhos

--- src/banco/tabelas.sql
+++ src/banco/tabelas.sql
@@ -32,3 +32,19 @@ CREATE TABLE transacoes (
   usuario_id integer references usuarios(id)
 );
 
+CREATE TABLE avaliacoes (
+  id SERIAL PRIMARY KEY,
+  produto_id INT NOT NULL REFERENCES produtos(id),
+  usuario_id INT NOT NULL,  -- Id do usuário que está fazendo a avaliação
+  nota INT CHECK (nota >= 1 AND nota <= 5),  -- Avaliação de 1 a 5
+  comentario TEXT,
+  data_avaliacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE carrinhos (
+  id SERIAL PRIMARY KEY,
+  usuario_id INT NOT NULL REFERENCES usuarios(id), -- Relacionado ao usuário
+  produto_id INT NOT NULL REFERENCES produtos(id), -- Relacionado ao produto
+  quantidade INT NOT NULL DEFAULT 1, -- Quantidade do produto no carrinho
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Data de inserção
+);
