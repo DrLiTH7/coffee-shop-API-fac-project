6875d14363f3ade4bb1d6bcc1f5ab56d537dda1b
DrLiTH7 <lith.yakame@gmail.com>
2024-11-20 16:32:51 -0300

    Alterado a tabela Usuario para que esta armazene todos os dados necessários à entidadede usuario (id, nome, telefone, email, rura, numero_casa, bairro, cidade, estado, cep, complemento)

--- src/banco/tabelas.sql
+++ src/banco/tabelas.sql
@@ -1,15 +1,25 @@
 CREATE DATABASE coffeemuchobom;
 
-CREATE TABLE usuarios (
-  id serial primary key,
-  nome text,
-  email text unique,
-  senha text
+CREATE TABLE usuario (
+    id serial primary key,
+    nome varchar(100) not null,
+    telefone varchar(15),
+    email varchar(100) unique not null,
+    rua varchar(100),
+    numero_casa varchar(10),
+    bairro varchar(100),
+    cidade varchar(100),
+    estado varchar(2),
+    cep varchar(10),
+    complemento varchar(100),
+    senha text not null
 );
+
 
 CREATE TABLE produtos (
 	id serial primary key,
-  produto, text,
+  produto text,
+  valor integer,
   descricao text
 );
 
